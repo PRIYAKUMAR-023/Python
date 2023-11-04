1765f1256b955b08d9d200ca1beb6f7579e172fb
PRIYAKUMAR-023 <143388619+PRIYAKUMAR-023@users.noreply.github.com>
2023-11-04 13:56:25 +0530

    If ,else

---
+++ inside of if.py
@@ -0,0 +1,8 @@
+Percentage=int(input("Enter your %:"))
+if(Percentage>=70):
+    Name=input("Enter your name:")
+    Age=int(input("Age="))
+    Department=input("Department:")
+    print("You are eligible")
+else:
+    print("You are not eligible")
