a1c2e9e08b1e73d3052dabf2deba4b7caaaa9cae
PRIYAKUMAR-023 <143388619+PRIYAKUMAR-023@users.noreply.github.com>
2023-11-04 12:41:19 +0530

    casting

---
+++ casting.py
@@ -0,0 +1,25 @@
+a=10
+b=20
+
+print(a+b)
+
+#casting=converting a data type to another data type 
+c=int("20")
+d=int("10")
+print(c+d)
+
+
+#user input
+
+x=int(input())
+y=int(input())
+z=x+y
+print(z)
+
+
+name=input()
+age=input()
+address=input()
+print("my name is:",name)
+print("my age is:",age)
+print("my address:",address)
