22ab796747908da845d95d3d6f9bc82b1ceb525f
PRIYAKUMAR-023 <143388619+PRIYAKUMAR-023@users.noreply.github.com>
2023-11-07 11:47:40 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,28 @@
+for i in "APPLE":
+    print(i)
+
+
+for n in range(3):
+    print(n)
+
+
+for n in range(2,6):
+    print(n)
+
+for n in range(1,11):
+    print(n,"X 2 =",n*2)
+for n  in range (1,11):
+    print(n,"x3=",n*3)
+
+
+
+
+a=int(input())
+b=int(input())
+for n in range(a,b):
+ print(n)
+
+
+for x in range(1,11):
+    if (x%2==0):
+        print(x)
