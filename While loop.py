a7f72adfe438c92904624785d0305ced32022380
PRIYAKUMAR-023 <143388619+PRIYAKUMAR-023@users.noreply.github.com>
2023-11-11 12:30:24 +0530

    Add files via upload

---
+++ While loop.py
@@ -0,0 +1,21 @@
+
+i=1
+while(i<=20):
+    print(i*10,end=",")
+    i=i+1
+
+
+
+i=10
+while(i>=1):
+    print(i)
+    i=i-1
+
+    
+
+i=int(input("Enter Your Value:"))
+fact=1
+while(i>0):
+    fact=fact*i
+    i=i-1
+print(fact)
