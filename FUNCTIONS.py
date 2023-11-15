4f94eff47d291476f28e979219da56dedf8bf2b6
PRIYAKUMAR-023 <143388619+PRIYAKUMAR-023@users.noreply.github.com>
2023-11-15 13:08:39 +0530

    Add files via upload

---
+++ FUNCTIONS.py
@@ -0,0 +1,51 @@
+"""def  add():
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print("total:",a+b)
+def  sub():
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print("total:",a-b)
+def  multiple():
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print("total:",a*b)
+def  div():
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print("total:",a/b)
+div()
+
+
+def findevenorodd():
+    if(num%2==0):
+        print("Even")
+    else:
+        print("odd")
+num=int(input("Enter the Number="))
+findevenorodd()
+
+add()
+
+def marks(total):
+    print("Annual mark=",total)
+marks(498)
+
+sub()
+
+
+def passorfail(mark):
+    if(mark>=35):
+        print("You are pass")
+    else:
+        print("Sorry!!!You are fail")
+mark=int(input("Enter Your Mark="))
+passorfail(mark)
+"""
+
+def rangeof(a,b):
+    for i in range(a,b):
+        print(i)
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+rangeof(a,b)
